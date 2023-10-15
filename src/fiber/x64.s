22230c624a6e13dcb6652fc6263cee453cfa5cae
ilikdoge <ilikdoge0@gmail.com>
2023-10-15 06:53:14 -0700

    organize

--- src/fiber/x64.s
+++ src/fiber/x64.s
@@ -1,36 +1,36 @@
 .global xx_core_fiber_x64_start
 xx_core_fiber_x64_start:
-	mov rdx, [rsp - 0x10]
+	mov rax, [rsp - 0x10]
 	mov rdi, [rsp - 0x08]
 	push qword ptr 0
-	jmp rdx
+	jmp rax
 
 .global xx_core_fiber_x64_intercept
 xx_core_fiber_x64_intercept:
-	mov rdx, [rsp - 0x18]
+	mov rax, [rsp - 0x18]
 	mov rdi, [rsp - 0x10]
 	sub rsp, 8
-	jmp rdx
+	jmp rax
 
 .global xx_core_fiber_x64_switch
 xx_core_fiber_x64_switch:
-	mov rdx, [rsp]
+	mov rax, [rsp]
 	add rsp, 8
 
 	mov [rdi + 0x00], r12
 	mov [rdi + 0x08], r13
 	mov [rdi + 0x10], r14
 	mov [rdi + 0x18], r15
-	mov [rdi + 0x20], rdx
+	mov [rdi + 0x20], rax
 	mov [rdi + 0x28], rsp
 	mov [rdi + 0x30], rbx
 	mov [rdi + 0x38], rbp
 
+	mov rax, [rsi + 0x20]
 	mov r12, [rsi + 0x00]
 	mov r13, [rsi + 0x08]
 	mov r14, [rsi + 0x10]
 	mov r15, [rsi + 0x18]
-	mov rax, [rsi + 0x20]
 	mov rsp, [rsi + 0x28]
 	mov rbx, [rsi + 0x30]
 	mov rbp, [rsi + 0x38]
--- src/task/block_on.rs
+++ src/task/block_on.rs
@@ -1,20 +1,25 @@
+use std::mem::{ManuallyDrop, MaybeUninit};
+
 use super::{Cancel, Progress, Request, RequestPtr, Task};
 use crate::pointer::{ConstPtr, MutPtr};
 
-type ResumeArg<Resume, Output> = (Resume, Option<Output>);
+type ResumeArg<Resume, Output> = (ManuallyDrop<Resume>, MaybeUninit<Output>);
 
-fn block_resume<Resume: FnMut(), Output>(_: RequestPtr<Output>, arg: *const (), value: Output) {
+fn block_resume<Resume: FnOnce(), Output>(_: RequestPtr<Output>, arg: *const (), value: Output) {
 	let mut arg: MutPtr<ResumeArg<Resume, Output>> = ConstPtr::from(arg).cast();
+	let resume = unsafe { ManuallyDrop::take(&mut arg.0) };
 
-	arg.1 = Some(value);
-	(arg.0)();
+	arg.1.write(value);
+
+	resume();
 }
 
+/// Safety: memory leak if `resume` is not called
 #[inline(always)]
-pub fn block_on<Block: FnMut(C), Resume: FnMut(), T: Task<Output, C>, C: Cancel, Output>(
-	mut block: Block, resume: Resume, task: T
+pub fn block_on<Block: FnOnce(C), Resume: FnOnce(), T: Task<Output, C>, C: Cancel, Output>(
+	block: Block, resume: Resume, task: T
 ) -> Output {
-	let mut arg: ResumeArg<Resume, Output> = (resume, None);
+	let mut arg: ResumeArg<Resume, Output> = (ManuallyDrop::new(resume), MaybeUninit::uninit());
 
 	unsafe {
 		let request = Request::new(
@@ -23,12 +28,10 @@ pub fn block_on<Block: FnMut(C), Resume: FnMut(), T: Task<Output, C>, C: Cancel,
 		);
 
 		match task.run(ConstPtr::from(&request)) {
-			Progress::Done(value) => return value,
-			Progress::Pending(cancel) => block(cancel)
+			Progress::Pending(cancel) => block(cancel),
+			Progress::Done(value) => return value
 		};
 	};
 
-	arg.1
-		.take()
-		.expect("Task did not finish after blocking call ended")
+	unsafe { arg.1.assume_init() }
 }
